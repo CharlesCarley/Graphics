5a8f4b62ff6f04564d65b940f5683c5039a6b5e5
CharlesCarley <snailrose@gmail.com>
2021-02-01 17:01:37 -0700

    Initial setup

--- CMake/Configure.cmake
+++ CMake/Configure.cmake
@@ -21,5 +21,45 @@
 include(StaticRuntime)
 set_static_runtime()
 
+option(Graphics_BUILD_WINDOW      "Compile standalone window system" ON)
+option(Graphics_USE_SDL           "Build with SDL" OFF)
+
+if (NOT Graphics_BUILD_WINDOW)
+    set(Graphics_USE_SDL OFF CACHE BOOL "Build with SDL" FORCE)
+endif()
+
+
+macro(DefineExternalTarget NAME GROUP PATH)
+    set(${NAME}_ExternalTarget      TRUE)
+    set(${NAME}_TargetFolders       TRUE)
+    set(${NAME}_TargetName          ${NAME})
+    set(${NAME}_TargetGroup         ${GROUP})
+    set(${NAME}_INCLUDE             ${PATH})
+    set(${NAME}_LIBRARY             ${${NAME}_TargetName})
+endmacro()
+
+if (Graphics_USE_SDL)
+    set(SDL_FOLDER Dependencies)
+    set(SDL_LIBRARY SDL2main SDL2-static)
+    set(SDL_INCLUDE ${Dependencies_PATH}/SDL/SDL)
+    set(Window_WITH_SDL TRUE)
+endif()
+
+
+set(Data2Array_FOLDER TRUE)
+set(Data2Array_FOLDER Extern)
+
+set(Dependencies_PATH ${CMAKE_SOURCE_DIR}/Extern)
+
+DefineExternalTarget(Utils         Extern "${Dependencies_PATH}")
+DefineExternalTarget(Math          Extern "${Dependencies_PATH}")
+DefineExternalTarget(FreeType      Extern "${Dependencies_PATH}/FreeType/Source/2.10.4/include")
+DefineExternalTarget(FreeImage     Extern "${Dependencies_PATH}/FreeImage/Source")
+DefineExternalTarget(Image         Extern "${Dependencies_PATH}/Image")
+DefineExternalTarget(Window        Extern "${Dependencies_PATH}/Window")
+
+find_package(OpenGL REQUIRED)
+set(OpenGL_LIB ${OPENGL_LIBRARIES})
+
 
 
