9f4e4225e284969a54d88e72f00bf8af7a9ae1f8
CharlesCarley <snailrose@gmail.com>
2021-04-04 20:58:52 -0600

    Update

--- Tests/CTest/CTest.c
+++ Tests/CTest/CTest.c
@@ -96,7 +96,7 @@ void WindowPaint(SKwindow window, void* user)
 
 void WindowMotion(SKwindow window, void* user, SKint32 x, SKint32 y)
 {
-    if (skGetMouseState(window, MBT_L) && skGetKeyCodeState(window, KC_CTRL))
+    if (skGetMouseState(window, MBT_L) || skGetKeyCodeState(window, KC_CTRL))
     {
         skGetMouseCo(window, &x, &y);
         UpdateMouseCo(user, x, y);
